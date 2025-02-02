af9524c8069dd5acc024529fa4d5e55922167a91
CDDing <jangmee9918@gmail.com>
2025-02-02 20:57:09 +0900

    fix Shader

--- Vulkan/shaders/compile.bat
+++ Vulkan/shaders/compile.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Vulkan SDK glslc 경로 설정
-set GLSLC_PATH=C:\VulkanSDK\1.3.261.1\Bin\glslc.exe
+set GLSLC_PATH=C:\VulkanSDK\1.4.304.0\Bin\glslc.exe
 
 :: 현재 디렉토리에서 모든 .vert와 .frag 파일 처리
 for %%f in (*.vert *.frag) do (
